8b53c5c9536d4fe69d311b4c1aceb5d1faf74e7e
Kevin Liu <kevinxo328@gmail.com>
2025-07-17 23:48:15 +0800

    Feat: move engine and factory initialization outside of get_db_session function

--- database/session.py
+++ database/session.py
@@ -9,10 +9,16 @@ from sqlalchemy.ext.asyncio import (
 
 from settings import DATABASE_URL
 
+engine = create_async_engine(DATABASE_URL, echo=True)
+factory = async_sessionmaker(engine)
+
 
 async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
-    engine = create_async_engine(DATABASE_URL, echo=True)
-    factory = async_sessionmaker(engine)
+    """
+    Get a database session.
+    This function provides an asynchronous session for database operations.
+    It ensures that the session is properly committed or rolled back in case of errors.
+    """
     async with factory() as session:
         try:
             yield session
